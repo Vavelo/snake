fc5e4bbe094c625241b2a69bc0c3e65e8c1025c3
Vavelo <bewikifuj@zep-hyr.com>
2018-09-19 10:08:23 +0500

    урок_2

---
+++ змейка/змейка/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace змейка
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
